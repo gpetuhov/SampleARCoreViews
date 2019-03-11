9dc5690980cf66d6396586efb5fbb3df0fa6321f
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-11 12:37:18 +0300

    Check OpenGL version

--- app/src/main/java/com/gpetuhov/android/samplearcoreviews/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcoreviews/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.gpetuhov.android.samplearcoreviews
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pawegio.kandroid.toast
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val MIN_OPENGL_VERSION = 3.0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return
+        }
+
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * Finishes the activity if Sceneform can not run
+     */
+    private fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            toast("Sceneform requires Android N or later")
+            activity.finish()
+            return false
+        }
+
+        val openGlVersionString = (activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+            .deviceConfigurationInfo
+            .glEsVersion
+
+        if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
+            toast("Sceneform requires OpenGL ES 3.0 or later")
+            activity.finish()
+            return false
+        }
+
+        return true
     }
 }
